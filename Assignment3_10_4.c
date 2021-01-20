6c1e32578ea07220977b79b149c04dd33e5170e7
OniiChan00 <77683049+OniiChan00@users.noreply.github.com>
2021-01-20 16:02:31 +0700

    Create Assignment3_10_4.c

---
+++ Assignment3_10_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+long long convert(int num);
+
+
+int main(void) {
+  int a;
+  scanf("%d",&a);
+  printf("%lld",convert(a));
+  return 0;
+}
+
+long long convert(int num)
+{
+  int r,i = 1;
+  long long bin = 0;
+  while(num != 0)
+  {
+    r = num % 2;
+    num /= 2;
+    bin += r * i;
+    i *= 10;
+  }
+  return bin;
+}
